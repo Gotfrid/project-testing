35141268fe7d402c4195951ea7c771ecb454c65d
Pavel Demin <paveldemin@me.com>
2025-04-27 11:05:26 +0100

    refactor: rely on env var for cypress base url

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000",
+    baseUrl: process.env.BASE_URL ?? "http://localhost:3000",
     video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
